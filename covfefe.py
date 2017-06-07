cbe2791b84efa845a531d593de67c5e08ac79ba3
Elbitty <conor123481@gmail.com>
2017-06-07 18:55:06 +0900

    Update covfefe.py

--- covfefe.py
+++ covfefe.py
@@ -6,8 +6,8 @@ covfefe-Challenge:
     Rules:
         1. Variables other than covfefe should NOT be used.
         2. Any string other than "covfefe" as defined in the argument should NOT be used.
-        3. Assignment operator {=}, Comparison operators {{== >= <=},},
-                Arithmetic operators { + - * / ** += -= *= /= } are also PROHIBITED.
+        3. Assignment operator {=}, Comparison operators {== >= <=},
+                Arithmetic operators {+ - * / ** += -= *= /=} are also PROHIBITED.
         4. All kind of programming languages are allowed.
 
 
@@ -17,7 +17,7 @@ covfefe-Challenge:
         1. covfefe 이외의 변수는 사용할 수 없음.
         2. 인수에 정의된 "covfefe" 이외의 문자열은 사용할 수 없음.
         3. 대입 연산자 {=}, 비교 연산자 {== >= <=},
-            산술 연산자 + - * / ** += -= *= /= 역시 금지.
+            산술 연산자 {+ - * / ** += -= *= /=} 역시 금지.
         4. 모든 프로그래밍 언어가 허용됨.
 
 """
